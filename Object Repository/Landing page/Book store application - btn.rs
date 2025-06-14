d97a31c0c378c629b615d63c6a105aad8885f153
m_hafidh_s <hafidhsanjaya2@gmail.com>
2025-06-14 17:40:33 +0700

    add new object on landing page

---
+++ Object Repository/Landing page/Book store application - btn.rs
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Book store application - btn</name>
+   <tag></tag>
+   <elementGuidId>0adbd817-6217-4471-abd6-44905beee3a4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;app&quot;]/div/div/div[2]/div/div[6]/div/div[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;app&quot;]/div/div/div[2]/div/div[6]/div/div[2]</value>
+      <webElementGuid>3e48269c-3667-407b-af26-80a77e630b95</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/Landing page/logo header - img.rs
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>logo header - img</name>
+   <tag></tag>
+   <elementGuidId>0265d1a9-14b4-416d-82ad-b807cf5567a8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;app&quot;]/header/a/img</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;app&quot;]/header/a/img</value>
+      <webElementGuid>c5cd7125-ee93-4c5f-bbd3-a1070ad92d9f</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
